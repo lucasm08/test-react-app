c4ef3692e089a3863140f9d2854d23ef0f02f35d
Lucas Gompou <gompou16@univmetiers.ci>
2020-01-18 03:11:44 -0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      wasm: {}
+    };
+  }
+
+  componentDidMount() {
+    this.loadWasm();
+  }
+
+  loadWasm = async () => {
+    try {
+      const wasm = await import("external");
+      this.setState({ wasm });
+    } catch (err) {
+      console.error(`Unexpected error in loadWasm. [Message: ${err.message}]`);
+    }
+  };
+
+  render() {
+    const { wasm = {} } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            <div>
+              Add:{" "}
+              <input
+                type="text"
+                onChange={e => this.setState({ addNum1: e.target.value })}
+              />{" "}
+              <input
+                type="text"
+                onChange={e => this.setState({ addNum2: e.target.value })}
+              />
+            </div>
+            <div>
+              Result:{" "}
+              {wasm.add &&
+                wasm.add(this.state.addNum1 || 0, this.state.addNum2 || 0)}
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
